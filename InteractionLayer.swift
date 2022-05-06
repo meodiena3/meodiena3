3314c058a0df3401fbcaeb13df0fe9e3613cf93c
meodiena3 <89651893+meodiena3@users.noreply.github.com>
2022-05-06 00:47:21 -0500

    Add files via upload

--- InteractionLayer.swift
+++ InteractionLayer.swift
@@ -1,25 +1,50 @@
 import Scenes
+import Igis
 
-/*
- This class is responsible for the interaction Layer.
- Internally, it maintains the RenderableEntities for this layer.
- */
-
-
-class InteractionLayer : Layer
+class InteractionLayer : Layer, KeyDownHandler
 {
-//    let ball = Ball()
-    let background = Background()
+    let paddleLeft = Paddle(rect:Rect(size:Size(width:10, height:100)))
+    let paddleRight = Paddle(rect:Rect(size:Size(width:10, height:100)))
 
     init()
     {
-        // Using a meaningful name can be helpful for debugging
         super.init(name:"Interaction")
 
-        // We insert our RenderableEntities in the constructor
-        insert(entity: background, at:.back)
-        //          insert(entity: ball, at: .front)
-        //          ball.changeVelocity(velocityX: 6, velocityY: 6)
+        insert(entity: paddleLeft, at: .front)
+        insert(entity: paddleRight, at: .front)
+    }
+
+    override func preSetup(canvasSize: Size, canvas: Canvas)
+    {
+        paddleLeft.move(to:Point(x: 100, y: 480))
+        paddleRight.move(to:Point(x: 1850, y: canvasSize.center.y))
+
+        dispatcher.registerKeyDownHandler(handler: self)
+
+    }
+
+    func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool)
+    {
+        let tlpl = paddleLeft.rectangle.rect.topLeft
+        let tlpr = paddleRight.rectangle.rect.topLeft
+        switch key
+        {
+        case "w":
+            paddleLeft.move(to:Point(x: tlpl.x, y:tlpl.y - 10))
+        case "s":
+            paddleLeft.move(to:Point(x: tlpl.x, y:tlpl.y + 10))
+        case "ArrowUp":
+            paddleRight.move(to:Point(x: tlpr.x, y:tlpr.y - 10))
+        case "ArrowDown":
+            paddleRight.move(to:Point(x: tlpr.x, y:tlpr.y + 10))
+        default:
+            break
+        }
+
+    }
+
+    override func postTeardown()
+    {
+        dispatcher.unregisterKeyDownHandler(handler: self)
     }
 }
-   
